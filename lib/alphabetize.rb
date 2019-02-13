5096ba726f7cf326de72e56fa13fbadd6830c53a
Mariam Jaludi <mariam.jaludi@gmail.com>
2019-02-13 21:37:25 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,7 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 def alphabetize(arr)
   # code here
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
   arr.sort_by do |word|
     word.split("").collect do |char|
       ESPERANTO_ALPHABET.index(char) #convert arr to num indexes
